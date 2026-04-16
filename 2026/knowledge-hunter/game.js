343d2bc74a49a5911800176b16e8f2f6b6dc9809
im63330471 <64909571+im63330471@users.noreply.github.com>
2026-04-16 21:41:01 +0800

    update 2026 kh game

--- 2026/knowledge-hunter/game.js
+++ 2026/knowledge-hunter/game.js
@@ -170,7 +170,7 @@
     const c = weightedRandom(creatures);
     inEncounter = { creature: c, attemptsLeft: 3, answer: c.answer };
     showEncounter();
-    console.log("Encounter's probability:", inEncounter.probability);
+    console.log("Encounter's probability:", inEncounter.creature.probability);
   }
 
   function catchCreature(caughtCreature) {
